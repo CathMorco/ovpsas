d2a601b6f95cc561663c9b5c3799c9382087ef60
mishal <mishalnorona014@gmail.com>
2026-01-28 23:04:51 +0800

    User Directory Front End and Route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/userslist', function () {
+    return view('userslist');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
